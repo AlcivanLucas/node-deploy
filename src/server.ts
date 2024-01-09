d4b18a4a377e4930ba534f4803a50befaf958785
AlcivanLucas <alcivan14lucas@gmail.com>
2024-01-09 12:43:02 -0300

    express, rotas funcionais: /day,/users, /disciplines, ate o momento a aplicação não consome a api express, e a rota /summary não retorna nada

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,23 @@
-
 import { appRoutes } from "./routes"
 import express from 'express';
 import cors from 'cors';
+//o CORS diz quais aplicações podem acessar a nossa API
+
+import dayjs from "dayjs"
+// import { FastifyInstance } from "fastify"
+import { Request, Response } from 'express'; // Importando Request e Response do Express
+import { z, AnyZodObject } from 'zod' // biblioteca para validação de dados
+import { prisma } from "./lib/prisma"
+
+
 
 const app = express();
 
-//o CORS diz quais aplicações podem acessar a nossa API
+app.use(express.json());
+
+// appRoutes(app);
+
+
 // const app = Fastify()
 // import Fastify from "fastify"
 // import cors from "@fastify/cors"
@@ -14,12 +26,10 @@ const app = express();
 //     origin: true,
 // })
 
-app.use(cors());
-
+// Registra as rotas
 // app.register(appRoutes)
 
-// Registra as rotas
-appRoutes(app);
+
 
 
 
@@ -34,7 +44,7 @@ const start = (): void => {
     }
   };
   start();
-
+// app.listen(3333, () => console.log("Server is running on port 3333"));
 
 
 
@@ -46,3 +56,226 @@ const start = (): void => {
 // })
 
 // sudo lsof -i :3333 mostra quais aplicações estão usando a porta 3333
+//  killall -9 node : mata todos os processos do node
+
+
+// app.post("/usuarios" ,(request,response) => {
+//   return response.json(request.body);
+// })
+// app.get("/", (req, res)=>{
+//   return res.json("servidor rodando por enquanto corretamente 🙏")
+// })
+
+app.post('/disciplines', async (request: Request, response: Response) => {
+  const createDisciplineBody = z.object({
+      title: z.string(),
+      weekDays: z.array(
+          z.number().min(0).max(6)
+      )
+
+  })
+
+  const { title, weekDays } = createDisciplineBody.parse(request.body)
+
+  //zera a hr, min, seg: 00:00:00, para que a disciplina apareça no msm dia que foi criada
+  const today = dayjs().startOf('day').toDate()
+
+  await prisma.discipline.create({
+      data: {
+          title,
+          created_at: today,
+          weekDays: {
+              create: weekDays.map(weekDay => {
+                  return {
+                      week_day: weekDay
+                  }
+              })
+          }
+      }
+  })
+   // Após criar a disciplina com sucesso, envie uma resposta de sucesso
+   return response.status(200).json({ message: 'Discipline created successfully' });
+})
+
+// rota responsável por criar usuários
+// app.post('/users', async (request) => {
+app.post('/users', async (request: Request, response: Response) => {
+  const createUserBody = z.object({
+      name: z.string(),
+      password: z.string(),
+      email: z.string().email(),
+  })
+
+  const { name,email,password} = createUserBody.parse(request.body)
+
+  //zera a hr, min, seg: 00:00:00, para que o user apareça no msm dia que foi criada
+  const today = dayjs().startOf('day').toDate()
+
+  await prisma.user.create({
+      data: {
+          name,
+          created_at: today,
+          email,
+          password,
+      }
+  })
+  await prisma.$disconnect();
+  // Após criar o usuário com sucesso, envie uma resposta de sucesso
+  return response.status(200).json({ message: 'User created successfully' });
+})
+
+// rota responsável deletar disciplinas *em desenvolvimento
+// app.delete('/deletedisciplines', async (request) => {
+app.delete('/deletedisciplines', async (request: Request, response: Response) => {
+  const deleteDisciplineParams = z.object({
+      id: z.string().uuid(),
+  })
+
+  const { id } = deleteDisciplineParams.parse(request.params)
+
+  await prisma.discipline.delete({
+      where: {
+          id
+      }
+  })
+})
+
+// rota responsável por busta hábitos de um dia específico
+// app.get('/day', async (request) => {
+app.get('/day', async (request: Request, response: Response) => {
+  const getDayParams = z.object({
+      date: z.coerce.date()
+  })
+
+  const { date } = getDayParams.parse(request.query)
+
+  const parsedDate = dayjs(date).startOf('day')
+
+  const weekDay = dayjs(parsedDate).get('day')
+
+  const possibleDisciplines = await prisma.discipline.findMany({
+      where: {
+          created_at: {
+              lte: date,
+          },
+          weekDays: {
+              some: {
+                  week_day: weekDay,
+              }
+          }
+      }
+  });
+
+  // Limpar a declaração preparada após usá-la
+  // Liberar a conexão com o banco de dados após usá-la
+  await prisma.$disconnect();
+
+  const day = await prisma.day.findUnique({
+      where: {
+          date: parsedDate.toDate(),
+      },
+      include: {
+          dayDisciplines: true
+      }
+  })
+
+  const completedDisciplines = day?.dayDisciplines.map(dayDiscipline => {
+      return dayDiscipline.discipline_id
+  })
+
+  return response.json({
+    possibleDisciplines,
+    completedDisciplines
+}); // Enviando a resposta ao cliente
+})
+
+// completar / não-completar um hábito, muda o status
+// caso queira mudar disciplinas retroativos assistir aula 03 minuto 7;00
+// app.patch('/disciplines/:id/toggle', async (request) => {
+app.patch('/disciplines/:id/toggle', async (request: Request, response: Response) => {
+
+  const toggleDisciplineParams = z.object({
+      id: z.string().uuid(),
+  })
+
+  const { id } = toggleDisciplineParams.parse(request.params)
+
+  const today = dayjs().startOf('day').toDate()
+
+  let day = await prisma.day.findUnique({
+      where: {
+          date: today
+      }
+  })
+
+  if (!day) {
+      day = await prisma.day.create({
+          data: {
+              date: today
+          }
+      })
+  }
+
+  const dayDiscipline = await prisma.dayDiscipline.findUnique({
+      where: {
+          day_id_discipline_id: {
+              day_id: day.id,
+              discipline_id: id
+          }
+      }
+  })
+
+  if (dayDiscipline) {
+      //remover a marcação de completo
+      await prisma.dayDiscipline.delete({
+          where: {
+              id: dayDiscipline.id,
+          }
+      })
+  } else {
+      // Completar o hábito
+      await prisma.dayDiscipline.create({
+          data: {
+              day_id: day.id,
+              discipline_id: id
+          }
+      })
+  }
+
+
+})
+// rota para buscar o sumario de disciplinas do dia especifico
+// app.get('/summary', async () => {
+app.get('/summary', async (request: Request, response: Response) => {
+  const summary = await prisma.$queryRaw`
+      SELECT 
+          D.id,
+          D.date,
+          (
+              SELECT 
+                 cast(count(*) as float)
+              FROM day_disciplines DH
+              WHERE DH.day_id = D.id 
+          ) as completed,
+          (
+              SELECT
+                  cast(count(*) as float)
+              FROM discipline_week_days HWD
+              JOIN disciplines H
+                  ON H.id = HWD.discipline_id
+              WHERE  
+                  HWD.week_day = date_part('dow', D.date) - 1
+                  AND H.created_at <= D.date
+          ) as amount
+      FROM days D
+  `
+  await prisma.$disconnect();
+
+  return summary
+})
+
+
+
+
+
+// ConnectorError(ConnectorError { user_facing_error: None, kind: QueryError(PostgresError { code: "42P05", message: "prepared statement "s0" already exists", severity: "ERROR", detail: None, column: None, hint: None }), transient: false })
