8546da60dcc132517cf2aa573d7e6ecdb941e710
AlcivanLucas <alcivan14lucas@gmail.com>
2024-01-21 15:52:38 -0300

    a

--- src/server.ts
+++ src/server.ts
@@ -29,12 +29,15 @@ const start = (): void => {
 
 
 // Configurar headers CORS para permitir qualquer origem
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-  });
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type');
+//     next();
+//   });
+
+  // Permitir qualquer origem
+app.use(cors());
 
 // app.listen({
 //     port: 3333,
