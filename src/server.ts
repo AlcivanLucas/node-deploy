a750bd64dd1a8579ed26bb5e318232cf71c31ceb
AlcivanLucas <alcivan14lucas@gmail.com>
2024-02-11 19:34:38 -0300

    adicionei o pactch no cors pra liberar a ação

--- src/server.ts
+++ src/server.ts
@@ -28,7 +28,7 @@ const start = (): void => {
 // Configurar headers CORS para permitir origens abertas
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
   });
